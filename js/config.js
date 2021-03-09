85791de5d016f52b1d12e8e987e2d35b32c66f20
Daniel Brambila <danielbrambila@Daniels-MacBook-Pro.local>
2021-03-08 16:33:28 -0800

    updated config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_vU7oj9ysX', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'oidjmb6i7iat6i1m5ingq9pb7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
